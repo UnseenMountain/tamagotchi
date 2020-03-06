277551df9a8b0770fdd4323294d1d30c1de147d6
Luke Klymshyn <unseenmountain@gmail.com>
2020-03-05 20:00:24 -0800

    seeds and mongoose npm packages

---
+++ scripts/seedDB.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const db = require("../models");
+
+mongoose.connect(
+        prosses.env.MONGOD_URI || "mongodb://localhost/tamagochi"
+);
+
+const petSeed = [
+    {
+        pet: "",
+        health: 5,
+        hatched: false,
+        level: 1,
+        attackPower: 1,
+        multiplayer: 1.5
+    }
+]
+
+const enemySeed = [
+    {
+        name: "",
+        health: 2,
+        level: 1,
+        attackPower: 2,
+        EXP: 3
+    }
+]
+
+module.exports = petSeed;
+
+module.exports = enemySeed;
