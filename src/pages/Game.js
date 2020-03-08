a9c58e90b6dce933f145c6bd59385b571ba26486
Luke Klymshyn <unseenmountain@gmail.com>
2020-03-07 18:43:24 -0800

    footer and navbar are created

---
+++ src/pages/Game.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Nav from '../components/Nav/Nav.js';
+import Footer from '../components/Footer/footer.js';
+import Menu from '../components/Menu/menu.js';
+
+
+const Game = () => {
+    return (
+        <> </>
+    );
+}
+
+export default Game;
