e07830b14bb1f54964f07b45b8481365eab38a47
Hyejin Kim <cshjkim@gmail.com>
2020-03-12 21:13:05 -0700

    working on okta 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,16 +6,25 @@ import Game from "./pages/Game.js"
 import Nav from './components/Nav/Nav.js';
 import Footer from './components/Footer/footer.js';
 import Menu from './components/Menu/menu.js';
-
-
+// import { SecureRoute, Security, LoginCallback } from '@okta/okta-react';
+// import Home from './Home';
+import LoginPage from './components/auth/LoginPage';
 
 
 function App() {
   return (
     <Router>
+       {/* <Security issuer='https://dev-243399.okta.com/oauth2/default'
+                  clientId='0oa33m68qMWbxYgrm4x6'
+                  redirectUri={window.location.origin + '/implicit/callback'} >
+          <Route path='/' exact={true} component={Home}/>
+          <SecureRoute path='/protected' component={Menu}/>
+          <Route path='/implicit/callback' component={LoginCallback} />
+        </Security> */}
       <div>
         <Nav />
         <Menu>
+          <Route path="/login" component={LoginPage} />
           <Route exact path="/" component={Game} />
           <Route exact path="/Game" component={Game}></Route>
         </Menu>
